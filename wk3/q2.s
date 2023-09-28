c412ef4c92ecac11887d8703af720e63ec14aeaf
Cassandra <cassandraeliot@student.unsw.edu.au>
2023-09-28 23:05:06 +1000

    Week 3: Pre-Tute commit

---
+++ wk3/q2.s
@@ -0,0 +1,9 @@
+
+
+	.data			# Enter the data section of memory
+				# int u;
+				# int v = 42;
+				# char w;
+				# char x = 'a';
+				# double y;
+				# int z[20];
---
+++ wk3/q8.s
@@ -0,0 +1,8 @@
+	.data
+
+get_str_len:
+	# s in $t0
+	# length in $t1
+
+string:
+	.asciiz "...."
